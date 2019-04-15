74b85887a2f8ba940a6ad8d60188e05683ab16f5
HunterWilkins <wilkins.hunter@gmail.com>
2019-04-15 11:34:54 -0500

    Added shark database thing

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,34 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"Thebleepingcat96!",
-    database: "ultimatedb"
-});
+
+
+if (process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    var connection = mysql.createConnection({
+        host:"localhost",
+        port:3306,
+        user:"root",
+        password:"Thebleepingcat96!",
+        database: "ultimatedb"
+    });
+}
+
+//EDIT THIS IF SOMETHING ISNT WORKING BC USUALLY THIS IS THE PROBLEM!!!!
+let connection;
+   if (process.env.JAWSDB_URL){
+       connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host: "localhost",
+            port: 8889, 
+            user: "root",
+            password: "root",
+            database: "burgers_db"
+        });
+    };
+
 
 connection.connect(function(err){
     if (err){
